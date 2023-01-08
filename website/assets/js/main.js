c37aa2f42160c3ff97b36ccec3500f48edf12592
Johan Daniel R Garcia <daniel.garcia@tambourine.com>
2023-01-08 16:42:50 -0500

    build: init commit

---
+++ website/assets/js/main.js
@@ -0,0 +1,15 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector('[data-node="video"]')
+const btnTogglePlay = document.querySelector('[data-node="btn-togglePlay"]')
+const btnToggleMute = document.querySelector('[data-node="btn-toggleMute"]')
+
+const onPlayer = new MediaPlayer({
+	selector: video,
+	plugins: [
+		//new AutoPlay()
+	],
+})
+btnTogglePlay.onclick = () => onPlayer.togglePlay()
+btnToggleMute.onclick = () => onPlayer.toggleMute()
